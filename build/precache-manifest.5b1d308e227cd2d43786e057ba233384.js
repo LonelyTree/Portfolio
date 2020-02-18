f06bc9f9e0f9d2dde27cb07513c2a0ea67e3551d
Alex Hughes <alexhughes@AlexMBPro.local>
2020-02-18 14:29:06 -0800

    created build for BlueHost

---
+++ build/precache-manifest.5b1d308e227cd2d43786e057ba233384.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30a73c3cc69e335bc80bfec983b5bee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7428811cfd8b8bd2d335",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "94356efe9124b01c6b47",
+    "url": "/static/css/main.8500495c.chunk.css"
+  },
+  {
+    "revision": "7428811cfd8b8bd2d335",
+    "url": "/static/js/2.57a238ca.chunk.js"
+  },
+  {
+    "revision": "cfdab193a95b91ea5e697ea867b7d7d0",
+    "url": "/static/js/2.57a238ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94356efe9124b01c6b47",
+    "url": "/static/js/main.8cb73e02.chunk.js"
+  },
+  {
+    "revision": "63565610d567df5fde47",
+    "url": "/static/js/runtime-main.bca7230b.js"
+  },
+  {
+    "revision": "37b88200e693ce75ec5eec97b99cd9ed",
+    "url": "/static/media/FroshsContact.37b88200.png"
+  },
+  {
+    "revision": "35fc1a6446c0d5d2b61ca5a6f9fb12b2",
+    "url": "/static/media/FroshsMain.35fc1a64.png"
+  },
+  {
+    "revision": "26277ac8713e4cbee6220622e00ac227",
+    "url": "/static/media/LI-China-In-Bug.26277ac8.png"
+  },
+  {
+    "revision": "dfadee7bf1e8e3ebfa88f47f876e77ee",
+    "url": "/static/media/LI-China-In-Square.dfadee7b.png"
+  },
+  {
+    "revision": "4c0d905b707134000970f569a282861e",
+    "url": "/static/media/LOGO_CSSt.4c0d905b.png"
+  },
+  {
+    "revision": "2fd7e7832dac84a8839e1e30c21c1377",
+    "url": "/static/media/LOGO_CSharp.2fd7e783.png"
+  },
+  {
+    "revision": "e495943ae77fc3a27e4b8d9bb037ef58",
+    "url": "/static/media/LOGO_Confetti.e495943a.png"
+  },
+  {
+    "revision": "d91c61c0e6129c6f5534f0f1a4bd0659",
+    "url": "/static/media/LOGO_Data_Set.d91c61c0.png"
+  },
+  {
+    "revision": "92a695e195ebc3e73b1c508f42690e10",
+    "url": "/static/media/LOGO_Express_js.92a695e1.png"
+  },
+  {
+    "revision": "03759b324d324a2cb0f442abc9b20435",
+    "url": "/static/media/LOGO_Google.03759b32.png"
+  },
+  {
+    "revision": "30229fd3852aaae8548abf5b7ac03be1",
+    "url": "/static/media/LOGO_HTML.30229fd3.png"
+  },
+  {
+    "revision": "7248033a2b7352f7132104fdaded5f83",
+    "url": "/static/media/LOGO_JavaScript.7248033a.png"
+  },
+  {
+    "revision": "ad52f9707f7fc265db6367041266b34a",
+    "url": "/static/media/LOGO_MLAgents.ad52f970.png"
+  },
+  {
+    "revision": "db10544963376d3a8366fd74c9992fd7",
+    "url": "/static/media/LOGO_MongoDB.db105449.png"
+  },
+  {
+    "revision": "c8b72c2962259add181a0b0e9c424385",
+    "url": "/static/media/LOGO_Mongoose.c8b72c29.png"
+  },
+  {
+    "revision": "f7f0f7670f365fba83cbf9d943addfc7",
+    "url": "/static/media/LOGO_NASA.f7f0f767.png"
+  },
+  {
+    "revision": "bdbad8924e01e9413306411b9c313e60",
+    "url": "/static/media/LOGO_Node.bdbad892.png"
+  },
+  {
+    "revision": "f02453eb515587b1164136da73ae6956",
+    "url": "/static/media/LOGO_Postgresql.f02453eb.png"
+  },
+  {
+    "revision": "71f750eca1657561811b9fa6a3e15500",
+    "url": "/static/media/LOGO_Python.71f750ec.png"
+  },
+  {
+    "revision": "c7b5aa8506abb5c98e4ad29892f8c922",
+    "url": "/static/media/LOGO_React.c7b5aa85.png"
+  },
+  {
+    "revision": "b957fecbccac8e9b9300ad0dd784d055",
+    "url": "/static/media/LOGO_React_Globe.b957fecb.png"
+  },
+  {
+    "revision": "ce159b6debb25d4d29a597991ebf2fd6",
+    "url": "/static/media/LOGO_React_Hooks.ce159b6d.png"
+  },
+  {
+    "revision": "bc234b8a60c0598ebd91cc138f00ba7a",
+    "url": "/static/media/LOGO_React_Spring.bc234b8a.png"
+  },
+  {
+    "revision": "0eaec546f78e4baf4ad2c76d4cbd0dff",
+    "url": "/static/media/LOGO_SASS.0eaec546.png"
+  },
+  {
+    "revision": "838432ae5a85e38251dcf5b1af6816b1",
+    "url": "/static/media/LOGO_StyledComponents.838432ae.png"
+  },
+  {
+    "revision": "26d0dee4e21b43c0a64f81622c7ce4b7",
+    "url": "/static/media/LOGO_Tensorflow.26d0dee4.png"
+  },
+  {
+    "revision": "80c42eebd54a01f2050842b4670baccf",
+    "url": "/static/media/LOGO_Unity3d.80c42eeb.png"
+  },
+  {
+    "revision": "9d4a74ac6ed07b523049317ba8857e0e",
+    "url": "/static/media/LOGO_Virtual_Sky.9d4a74ac.png"
+  },
+  {
+    "revision": "5efea7afca90136371784ecf0c3b2b7f",
+    "url": "/static/media/LOGO_jQuery.5efea7af.png"
+  },
+  {
+    "revision": "579c307efc9035a0ee9e4ebaca909f20",
+    "url": "/static/media/alexHughesResume.579c307e.pdf"
+  },
+  {
+    "revision": "4a0f315590a03e174c3c331243e9bbdd",
+    "url": "/static/media/contagionControlMapMini.4a0f3155.png"
+  },
+  {
+    "revision": "f4258bf82d8a0c1aade7b58ffdbd036f",
+    "url": "/static/media/contagionControlMenuMini.f4258bf8.png"
+  },
+  {
+    "revision": "669dc1a6195918a050b7e08e496098c6",
+    "url": "/static/media/curve_arrow.669dc1a6.png"
+  },
+  {
+    "revision": "68d6ad952e891d9f1cbd0cc33dbd9715",
+    "url": "/static/media/giraffe.68d6ad95.png"
+  },
+  {
+    "revision": "e32927ead076632280f884249a8c6385",
+    "url": "/static/media/globalMenuMini.e32927ea.png"
+  },
+  {
+    "revision": "e52c1f6614526fcbc235bc1a65b06940",
+    "url": "/static/media/globalTitleMini.e52c1f66.png"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/static/media/glyph-logo_May2016.0e84d592.png"
+  },
+  {
+    "revision": "27047e50998d46b8dd190e1c71122f84",
+    "url": "/static/media/neuroDromeGameMini.27047e50.png"
+  },
+  {
+    "revision": "02422ce76af6ab4c06c9a1a81a02b4a4",
+    "url": "/static/media/neuroDromeMenuMini.02422ce7.png"
+  },
+  {
+    "revision": "eca7b3f7e1e0af7c2262977f8ca40f11",
+    "url": "/static/media/safeParkingMenuMini.eca7b3f7.png"
+  },
+  {
+    "revision": "ab290c28029cd73f5121370fc6aa09d8",
+    "url": "/static/media/safeParkingSearchMini.ab290c28.png"
+  },
+  {
+    "revision": "185c9578d6fa049b9739d2d21b833e07",
+    "url": "/static/media/scifiAction.185c9578.png"
+  },
+  {
+    "revision": "edfe123f5f77508e48d7e7e117be0d26",
+    "url": "/static/media/scifiMenu.edfe123f.png"
+  }
+]);
